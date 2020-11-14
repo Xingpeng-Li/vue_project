d273599082e45470d84ef2a934aa8a25823540bd
xubic <2979077422@qq.com>
2020-11-15 00:26:24 +0800

    添加了LoginController对应的js

---
+++ office_system_vue/src/api/login.js
@@ -0,0 +1,32 @@
+import request from '../util/request'
+
+export function login(phoneNumber,verificationCode) {
+  return request({
+    url: "/loginByPhone",
+    method: "post",
+    data:{
+      'phoneNumber': phoneNumber,
+      'verificationCode': verificationCode
+    }
+  })
+}
+
+export function loginByPassword(phoneNumber,password) {
+  return request({
+    url: "login",
+    method: "post",
+    params: {
+      'phoneNumber': phoneNumber,
+      'password': password
+    }
+  })
+}
+
+export function logout() {
+  return request({
+    url: "/logout",
+    method: "get",
+    params: {
+    }
+  })
+}
