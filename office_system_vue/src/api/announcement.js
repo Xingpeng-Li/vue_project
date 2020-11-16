f157a1b877f6ef0d999a8e70aec87f884c7f2f37
guiyi-210 <2021417370@qq.com>
2020-11-16 21:36:09 +0800

    把那些controller对应的api push一下

--- office_system_vue/src/api/announcement.js
+++ office_system_vue/src/api/announcement.js
@@ -1,12 +1,26 @@
-import request from '../util/request'
 
-export function select(keywords) {
-  //查询接口
+import request from "../util/request"
+
+export function sendAnnouncement(type,title,body) {
   return request({
     url: "/announcement/send",
     method: "post",
     data: {
-      'keywords':keywords
+      'type': type,
+      'title': title,
+      'body': body
     }
   })
 }
+
+export function getAnnouncement(type) {
+  return request({
+    url: "/announcement/get",
+    method: "get",
+    params: {
+      'type': type
+    }
+  })
+}
+
+
